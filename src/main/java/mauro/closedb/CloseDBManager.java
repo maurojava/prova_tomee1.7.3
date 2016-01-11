4c440f6c708e486517090c9f80711f10a60b25d4
mauro <mauro@server.fake>
2016-01-11 21:42:47 +0100

    commit terminated localizated  into italian language

---
+++ src/main/java/mauro/closedb/CloseDBManager.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mauro.closedb;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author mauro
+ */
+@Singleton
+@Startup
+public class CloseDBManager {
+   @PersistenceContext(unitName = "book-pu")
+    private EntityManager entityManager;
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+@PreDestroy
+public void closeDB(){
+    try{
+entityManager.createNativeQuery("SHUTDOWN").executeUpdate();
+    }
+    catch(Exception e){
+    e.printStackTrace();
+    }
+    if(entityManager!=null){entityManager.close();
+    }
+}
+
+  
+}
---
+++ src/main/java/org/primefaces/showcase/view/overlay/DialogView.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.primefaces.showcase.view.overlay;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author mauro
+ */
+@Named(value = "dialogView")
+@RequestScoped
+public class DialogView {
+
+     public void destroyWorld() {
+        addMessage("System Error", "Please try again later.");
+    }
+     
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+    
+}
